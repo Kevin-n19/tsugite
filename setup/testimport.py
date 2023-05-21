170efde7416a6777104bed064a3d87798c794301
vboxuser <vboxuser@Ubuntu.myguest.virtualbox.org>
2023-05-21 07:39:52 +0200

    Version1 Biesse CIX

---
+++ setup/testimport.py
@@ -0,0 +1,25 @@
+
+
+import sys
+import numpy as np
+import time
+import math
+import os
+
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5.uic import *
+from PyQt5.QtOpenGL import *
+
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+from math import tan, pi
+from Types import Types
+from Geometries import Geometries
+from ViewSettings import ViewSettings
+from Show import Show
+from _mainWindow import get_untitled_filename
+
+print('Ok')
